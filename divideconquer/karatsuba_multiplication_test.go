6519a8d254a7ea449347272cba20d494c698715a
Joshua Steven Theopillus <joshua.steven@tokopedia.com>
2017-01-31 12:50:07 +0700

    complete unittest for karatsuba multiplication

--- divideconquer/karatsuba_multiplication_test.go
+++ divideconquer/karatsuba_multiplication_test.go
@@ -7,6 +7,15 @@ import (
 )
 
 func TestKaratsubaMult(t *testing.T) {
-	a, b := -123, 4571
+	//positive positive
+	a, b := 123, 4571
+	assert.Equal(t, a*b, KaratsubaMult(a, b))
+
+	//positive negative
+	a, b = -123, 4571
+	assert.Equal(t, a*b, KaratsubaMult(a, b))
+
+	//negative negative
+	a, b = -123, -4571
 	assert.Equal(t, a*b, KaratsubaMult(a, b))
 }
