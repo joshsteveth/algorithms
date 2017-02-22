5f2538d03ef5ae0b11694e8aa4342248873fab1d
Joshua Steven Theopillus <joshua.steven@tokopedia.com>
2017-02-22 09:24:18 +0700

    add recursive karatsube and string operations

--- divideconquer/karatsuba_multiplication.go
+++ divideconquer/karatsuba_multiplication.go
@@ -1,5 +1,5 @@
 //Separate x into x1. B^m + x0
-//and y into y1.B*m + y0
+//and y into y1.B^m + y0
 //calculate z2 = x1.y1
 //calculate z0 = x0.y0
 //calculate z1 = (x1 + x0).(y1 + y0) - z0 - z2
@@ -58,4 +58,260 @@ func separateInts(a, m int) (int, int) {
 	x1, _ := strconv.Atoi(intStr[:len(intStr)-m])
 	x0, _ := strconv.Atoi(intStr[len(intStr)-m:])
 	return x1, x0
+}
+
+//recursive mode
+//make sure it only ends in 1 digt * 1 digt multiplication
+func karatsuba(a, b string) string {
+	if len(a) <= 4 || len(b) <= 4 {
+		return multString(a, b)
+	}
+
+	m := getMValueString(a, b)
+	fmt.Printf("m: %d, a: %s, b: %s\n", m, a, b)
+
+	//separate x into x1 and x0
+	x1, x0 := separateIntsString(a, m)
+	y1, y0 := separateIntsString(b, m)
+
+	z2, z0 := karatsuba(x1, y1), karatsuba(x0, y0)
+	z1 := karatsuba(addString(x1, x0), addString(y1, y0))
+	z1 = deductString(z1, z0)
+	z1 = deductString(z1, z2)
+
+	result1 := multStringLong(z2, powerOf("1", m*2))
+	result2 := multStringLong(z1, powerOf("1", m))
+
+	result := addString(result1, result2)
+	fmt.Println(result)
+	result = addString(result, z0)
+	fmt.Println(result)
+	return result
+}
+
+func getMValueString(a, b string) int {
+	l := len(a)
+	if len(b) < len(a) {
+		l = len(b)
+	}
+	return l / 2
+}
+
+func separateIntsString(a string, m int) (string, string) {
+	return a[:len(a)-m], a[len(a)-m:]
+}
+
+func powerOf(a string, pow int) string {
+	for i := 0; i < pow; i++ {
+		a += "0"
+	}
+	return a
+}
+
+func addString(a, b string) string {
+	ma := map[int]int{}
+	mb := map[int]int{}
+
+	for i := 0; i < len(a); i++ {
+		val, _ := strconv.Atoi(a[len(a)-1-i : len(a)-i])
+		ma[i] = val
+	}
+
+	for i := 0; i < len(b); i++ {
+		val, _ := strconv.Atoi(b[len(b)-1-i : len(b)-i])
+		mb[i] = val
+	}
+
+	l := len(ma)
+	if len(ma) < len(mb) {
+		l = len(mb)
+	}
+
+	mresult := map[int]int{}
+	var overflow int
+	var isOverflow bool
+
+	for i := 0; i < l; i++ {
+		var result int
+		if isOverflow {
+			result = overflow
+		}
+		isOverflow = false
+
+		if val, ok := ma[i]; ok {
+			result += val
+		}
+		if val, ok := mb[i]; ok {
+			result += val
+		}
+
+		if result > 9 {
+			isOverflow = true
+			resultStr := fmt.Sprintf("%d", result)
+
+			overflow, _ = strconv.Atoi(string(resultStr[0]))
+			result, _ = strconv.Atoi(string(resultStr[1]))
+		}
+
+		mresult[i] = result
+	}
+
+	if isOverflow {
+		mresult[l] = overflow
+		l++
+	}
+
+	var str string
+	for i := 0; i < l; i++ {
+		str += fmt.Sprintf("%d", mresult[l-1-i])
+	}
+	return str
+}
+
+func deductString(a, b string) string {
+	ma, mb := map[int]int{}, map[int]int{}
+
+	var isNeg bool
+
+	if less(a, b) {
+		a, b = b, a
+		isNeg = true
+	}
+
+	for i := 0; i < len(a); i++ {
+		val, _ := strconv.Atoi(a[len(a)-1-i : len(a)-i])
+		ma[i] = val
+	}
+
+	for i := 0; i < len(b); i++ {
+		val, _ := strconv.Atoi(b[len(b)-1-i : len(b)-i])
+		mb[i] = val
+	}
+
+	l := len(ma)
+	if len(ma) < len(mb) {
+		l = len(mb)
+	}
+
+	mresult := map[int]int{}
+	var overflow int
+	var isOverflow bool
+
+	for i := 0; i < l; i++ {
+		var result int
+		if isOverflow {
+			result -= overflow
+		}
+		isOverflow = false
+
+		if val, ok := ma[i]; ok {
+			result += val
+		}
+		if val, ok := mb[i]; ok {
+			result -= val
+		}
+
+		if result < 0 {
+			isOverflow = true
+
+			overflow = 1
+			result += 10
+		}
+
+		mresult[i] = result
+	}
+
+	if isOverflow {
+		mresult[l] = overflow
+		l++
+	}
+
+	var str string
+	for i := 0; i < l; i++ {
+		str += fmt.Sprintf("%d", mresult[l-1-i])
+	}
+
+	if isNeg {
+		str = fmt.Sprintf("-%s", str)
+	}
+
+	return str
+}
+
+func less(a, b string) bool {
+	if len(a) < len(b) {
+		return true
+	} else if len(a) > len(b) {
+		return false
+	}
+
+	ma, mb := map[int]int{}, map[int]int{}
+
+	for i := 0; i < len(a); i++ {
+		val, _ := strconv.Atoi(a[len(a)-1-i : len(a)-i])
+		ma[i] = val
+	}
+
+	for i := 0; i < len(b); i++ {
+		val, _ := strconv.Atoi(b[len(b)-1-i : len(b)-i])
+		mb[i] = val
+	}
+
+	for i := 0; i < len(ma); i++ {
+		if ma[i] < mb[i] {
+			return true
+		} else if ma[i] > mb[i] {
+			return false
+		}
+	}
+
+	return false
+}
+
+//function to multiply only limited number of string
+func multString(a, b string) string {
+	a2, _ := strconv.Atoi(a)
+	b2, _ := strconv.Atoi(b)
+	var c int
+	for i := 0; i < b2; i++ {
+		c += a2
+	}
+	return fmt.Sprintf("%d", c)
+}
+
+//support long multiplication, however b only consists of 1 element
+//create series of integer using map for a and then multiply it one by one
+//the order between a and b matters
+func multStringLongSingle(a, b string) string {
+	var result string
+
+	ma := map[int]int{}
+	for i := 0; i < len(a); i++ {
+		val, _ := strconv.Atoi(a[len(a)-1-i : len(a)-i])
+		ma[i] = val
+	}
+
+	for i := 0; i < len(a); i++ {
+		result = addString(result, powerOf(multString(b, fmt.Sprintf("%d", ma[i])), i))
+	}
+
+	return result
+}
+
+func multStringLong(a, b string) string {
+	var result string
+
+	mb := map[int]int{}
+	for i := 0; i < len(b); i++ {
+		val, _ := strconv.Atoi(b[len(b)-1-i : len(b)-i])
+		mb[i] = val
+	}
+
+	for i := 0; i < len(b); i++ {
+		singleMultResult := multStringLongSingle(a, fmt.Sprintf("%d", mb[i]))
+		powered := powerOf(singleMultResult, i)
+		result = addString(result, powered)
+	}
+
+	return result
 }
--- divideconquer/karatsuba_multiplication_test.go
+++ divideconquer/karatsuba_multiplication_test.go
@@ -1,6 +1,7 @@
 package divcon
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +19,86 @@ func TestKaratsubaMult(t *testing.T) {
 	//negative negative
 	a, b = -123, -4571
 	assert.Equal(t, a*b, KaratsubaMult(a, b))
+}
+
+func TestPowerOf(t *testing.T) {
+	t.Skip()
+	expected := "52000"
+	result := powerOf("52", 3)
+	assert.Equal(t, expected, result)
+}
+
+func TestAddString(t *testing.T) {
+	t.Skip()
+	a, b := "49200", "68888"
+	expected := "118088"
+	assert.Equal(t, expected, addString(a, b))
+}
+
+func TestAtomicMult(t *testing.T) {
+	t.Skip()
+	a, b := "3", "4"
+	expected := "12"
+	assert.Equal(t, expected, multString(a, b))
+}
+
+func TestMultStringLongSingle(t *testing.T) {
+	t.Skip()
+	a, b := "123", "4"
+	expected := "492"
+	assert.Equal(t, expected, multStringLongSingle(a, b))
+}
+
+func TestMultStringLong(t *testing.T) {
+	t.Skip()
+	a, b := "123", "456"
+	expected := fmt.Sprintf("%d", 123*456)
+	assert.Equal(t, expected, multStringLong(a, b))
+}
+
+func TestProgrammingAssignment(t *testing.T) {
+	t.Skip()
+	a := "3141592653589793238462643383279502884197169399375105820974944592"
+	b := "2718281828459045235360287471352662497757247093699959574966967627"
+
+	t.Log(multStringLong(a, b))
+}
+
+func TestDeductString(t *testing.T) {
+	a, b := "5301", "25"
+	expected := "5276"
+	assert.Equal(t, expected, deductString(a, b))
+
+	expected = "-5276"
+	assert.Equal(t, expected, deductString(b, a))
+}
+
+func TestLess(t *testing.T) {
+	a, b := "25", "531123"
+	assert.Equal(t, true, less(a, b))
+	assert.Equal(t, false, less(b, a))
+
+	a, b = "501", "503"
+	assert.Equal(t, true, less(a, b))
+	assert.Equal(t, false, less(b, a))
+}
+
+func TestKaratsuba(t *testing.T) {
+	//t.Skip()
+	a := "7315"
+	b := "123"
+	expected := "13259436476"
+	assert.Equal(t, expected, karatsuba(a, b))
+}
+
+func TestSeparateIntsString(t *testing.T) {
+	a := "5301"
+	exp1, exp2 := separateIntsString(a, 2)
+	assert.Equal(t, "53", exp1)
+	assert.Equal(t, "01", exp2)
+
+	b := "250"
+	exp1, exp2 = separateIntsString(b, 2)
+	assert.Equal(t, "2", exp1)
+	assert.Equal(t, "50", exp2)
 }
